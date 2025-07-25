3c81a4276891a3dd6f22d94bcbfeca98c8bfb009
JupiterPi <hello@jupiterpi.de>
2025-07-25 08:15:45 +0200

    tests

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,3 +1,36 @@
 import test from 'ava'
+import { setPassword, getPassword, deletePassword } from "../index.js"
 
-test.skip("No tests yet", () => {})
+test("should find no password", t => {
+    try {
+        getPassword("test-service", "test-account")
+        console.log("Password found, but it should not exist.")
+        t.fail()
+    } catch (error) {
+        t.pass()
+    }
+})
+
+test("should set a password", t => {
+    setPassword("test-service", "test-account", "test-password")
+    t.pass()
+})
+
+test("should get a password", t => {
+    const password = getPassword("test-service", "test-account")
+    t.is(password, "test-password")
+})
+
+test("should delete a password", t => {
+    deletePassword("test-service", "test-account")
+    t.pass()
+})
+
+test("should find no password after deletion", t => {
+    try {
+        getPassword("test-service", "test-account")
+        t.fail()
+    } catch (error) {
+        t.pass()
+    }
+})
